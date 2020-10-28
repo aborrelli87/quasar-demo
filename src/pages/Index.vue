64fca06c887ba48647d0233bd6f5feca61c8d0d0
aborrelli87 <a.borrelli87@gmail.com>
2020-10-28 01:10:47 +0100

    Prima installazione

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+    <div>{{prova}}</div>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    name: 'PageIndex',
+    data () {
+      return {
+        // notice String as value
+        prova: '16px'
+      }
+    }
+  }
+</script>
